3ef3bf38c069f7b442bf73f95ff78f36652cd6ee
malmsteen08 <neziryurekli@gmail.com>
2014-03-21 18:40:29 +0200

    TranslateApi

---
+++ default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="DilDeneme._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1 {
+            width: 100%;
+        }
+
+        .style2 {
+            width: 201px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="style1">
+
+                <tr>
+                    <td class="style2">Kelime Gir:</td>
+                    <td>
+                        <asp:TextBox ID="txttext" runat="server" TextMode="MultiLine"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style2">z:</td>
+                    <td>
+                        <asp:DropDownList ID="ddltolang" runat="server">
+                            <asp:ListItem Value="en">English</asp:ListItem>
+                            <asp:ListItem Value="fr">French</asp:ListItem>
+                            <asp:ListItem Value="es">Spanish</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style2">&nbsp;</td>
+                    <td>&nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Translate" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DilDeneme
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+            }
+        }
+
+        private void TranslateText()
+        {
+            string appId = "<Vf/AEaHym/fzxX19jwote9qzJ9Exr63RE+wRNls9Eho>";
+            string fromLang = "tr";
+            string translatedText = "";
+            string tobetranslated = txttext.Text;
+            string toLang = ddltolang.SelectedValue.ToString();
+            if (tobetranslated == "")
+            {
+
+            }
+            else
+            {
+                string uri = "http://api.microsofttranslator.com/v2/Http.svc/Translate?appId=" + appId + "&text=" + tobetranslated + "&from=" + fromLang + "&to=" + toLang;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                WebResponse response = request.GetResponse();
+                Stream strm = response.GetResponseStream();
+                StreamReader reader = new System.IO.StreamReader(strm);
+                translatedText = reader.ReadToEnd();
+                Response.Write("The translated text is: '" + translatedText + "'.");
+                response.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TranslateText();
+        }
+    }
+}
